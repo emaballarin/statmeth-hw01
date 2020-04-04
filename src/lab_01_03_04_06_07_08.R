2270e25b1384602953dd6d9114030ef226d836ed
Alice <alice.vegliach@gmail.com>
2020-04-04 16:32:39 +0200

    lab 8

--- src/lab_01_03_04_06_07_08.R
+++ src/lab_01_03_04_06_07_08.R
@@ -111,3 +111,34 @@ qqplot(qcauchy(ppoints(n),0, 1),y4,
        xlab="True quantiles", ylab="Sample quantiles",
        main = "Q-Q plot for Cauchy(0,1): n=100")
 qqline(y4, distribution = function(p) qcauchy(p, 0, 1), col="red")
+
+
+#### EX 8 ########
+
+clt_function <- function(n, dist){
+
+    x<-seq(1, n)
+
+    sample <- switch(dist,
+                     "beta" = rbeta(x, 2, 5),
+                     "binomial" = rbinom(x, n, 0.5),
+                     "chisquared" = rchisq(x, 5),
+                     "gamma" = rgamma(x, 3),
+                     "negativebinomial" = rnbinom(x, 3, 0.5),
+                     "tstudent" = rt(x, 5))
+
+    # plot the distribution of the mean of the observations
+    hist(sample, freq=FALSE, main="CLT", border="red", nclass=50)
+    # plot(density(sample), col="red", lwd = 1, main ="CLT")
+
+    # normal
+    mu = mean(sample)
+    var = var(sample)
+    curve(dnorm(x, mu, sqrt(var)), add= TRUE, col="black", lwd=2)
+}
+
+par(mfrow=c(2,2))
+clt_function(1000, "beta")
+clt_function(1000, "binomial")
+clt_function(1000, "chisquared")
+clt_function(1000, "tstudent")
